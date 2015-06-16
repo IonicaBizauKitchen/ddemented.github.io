659c78b848aef0cd95e6b0bdfa2e4a790c3ac8a7
Sanjeev S Nair <sanjeevsureshn@gmail.com>
2015-06-16 19:12:12 +0300

    bug-2

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     setTimeout( function(){
         //  Fade out loader
         $("#loader").toggle("clip");
-        }, 3000);
+      }, 1000);
 
     $(window).scroll(function(e){
         navFn();
