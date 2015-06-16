72e86a4c1f86d214194bbcb4b61251268e294a22
Sanjeev S Nair <sanjeevsureshn@gmail.com>
2015-06-16 19:37:36 +0300

    bug-1.1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
-    setTimeout( function(){
-        //  Fade out loader
-        $(".loader").toggle("clip");
-      }, 1000);
+  setTimeout( function(){
+    //  Fade out loader
+    $(".loader").fadeOut( 2000, "easeInOutExpo" );
+    }, 3000);
 
     $(window).scroll(function(e){
         navFn();
